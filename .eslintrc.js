e308568715ba5469070b45b1cdcb0889a88a021b
leesuin <suin0212@gmail.com>
2022-02-27 01:10:20 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:react/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-var": "error",
+    "func-style": ["error", "declaration", { allowArrowFunctions: true }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  globals: {
+    __DEV__: true,
+  },
+};
