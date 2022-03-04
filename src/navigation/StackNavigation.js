5176c973bf96baa5246cb6d2726e9eaa71df6549
leesuin <suin0212@gmail.com>
2022-03-04 10:37:42 +0900

    feat: separate stack

--- src/navigation/StackNavigation.js
+++ src/navigation/StackNavigation.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "../screens/LoginScreen";
 import PlanListScreen from "../screens/PlanListScreen";
+import asyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react/cjs/react.development";
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
+  const [user, setUser] = useState("");
+
+  const checkUser = async () => {
+    const userId = await asyncStorage.getItem("userId");
+    setUser(userId);
+  };
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="PlanList" component={PlanListScreen} />
+      {!user ? (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      ) : (
+        <Stack.Screen name="PlanList" component={PlanListScreen} />
+      )}
     </Stack.Navigator>
   );
 };
